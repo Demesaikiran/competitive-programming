a61afd4552ba8d27bec3abc9882642d81b22342a
demesaikiran <demesaikiran@gmail.com>
2021-08-02 18:07:05 +0530

    Garden Update

--- Leet Code/1954_MinimumGardenPerimetertoCollectEnoughApples.cpp
+++ Leet Code/1954_MinimumGardenPerimetertoCollectEnoughApples.cpp
@@ -8,20 +8,17 @@ public:
         int index =1;
         long long octa = 0;
         
-        
         while(neededApples > prev) {
             
-            
+            // MOTO: previous - (index -1) + (index * 2) + ((index * 2) -1)
             octa = octa + 3*index;
-            
-            
+
+            //Octa part of a square and multiplying with 8 and removing repeated apples [Along axis] [Corner Apples]
             long long squareplot = octa * 8 - (index * 8) - (index * 4);
             prev = prev + squareplot;
             
             index++;
-            
         }
-        
         return 8 * (index-1);
     }
 };
