8b846c73dd45a17e333385d6e8cabe1c330dc0d7
demesaikiran <demesaikiran@gmail.com>
2022-09-11 16:22:55 +0530

    Code: Sort colors & Counting Pairs

---
+++ GFG/Z_Programs/number_of_ways.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int number_of_paths(int arr[][], int m, int n)
+{
+    
+}
+
+int main()
+{
+    int arr[2][3] = {
+                        {1, 1, 0},
+                        {1, 1, 1}
+    };
+
+    printf("%d", number_of_paths(arr, 2, 3));
+
+
+}
