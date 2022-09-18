386c97bb62bf9de09dccfb577893921354348785
demesaikiran <demesaikiran@gmail.com>
2022-09-18 12:07:41 +0530

    Samsung prep

---
+++ GFG/Z_Programs/Samsung/Kadanes.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        if(n == 0) return 0;
+        
+        long long sum = arr[0], maxi = arr[0];
+        
+        for(int i = 1; i <n; i++)
+        {
+            sum = max((long long)arr[i], sum + arr[i]);
+            maxi = max(sum, maxi);
+
+        }
+        
+        return sum > maxi ? sum : maxi;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
